465b7b1a221f29e811b57129b5604e3ead7da955
Isaque Barbosa <isaquebatista400@gmail.com>
2025-07-08 04:02:47 -0300

    :sparkles: feat: add Routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ContactsComponent } from './components/contacts/contacts.component';
+import { ContactFormComponent } from './components/contact-form/contact-form.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: ContactsComponent},
+  {path: 'new', component: ContactFormComponent},
+  {path: 'edit/:id', component: ContactFormComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
